a10040153200f56b4254fd8dbc9aa908b2d9a10e
hyoseongLee <hyoseong123@naver.com>
2024-06-13 20:06:00 +0900

    react_0613

---
+++ frontend/src/component/Box.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../style/Box.css';
+const Box = () => {
+    return (
+        <div className='Boxcss'>
+ hihihihi hahaha
+        </div>
+    )
+}
+export default Box;
