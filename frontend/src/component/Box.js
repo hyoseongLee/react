603f58a3bc6201fac176de833a38abf1237f504d
hyoseongLee <hyoseong123@naver.com>
2024-06-14 21:07:32 +0900

    react0614

--- frontend/src/component/Box.js
+++ frontend/src/component/Box.js
@@ -2,8 +2,15 @@ import React from 'react';
 import '../style/Box.css';
 const Box = () => {
     return (
-        <div className='Boxcss'>
- hihihihi hahaha
+        <div className='headerbox'>
+            <div className='subbox'>
+                <div className='Dooray'>Dooray!</div>
+                |
+                 <div className='calender'>캘린더</div>
+
+            </div>
+
+            <div className='right'> 문의하기 </div>
         </div>
     )
 }
