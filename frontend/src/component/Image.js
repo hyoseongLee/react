df9af960610b8571c7491a2c258257be60ab40ab
hyoseongLee <hyoseong123@naver.com>
2024-06-24 21:19:42 +0900

    react Router_0624

---
+++ frontend/src/component/Image.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../style/Image.css';
+import { Link } from 'react-router-dom';
+
+const Image = () => {
+    return (
+        <div className='Imagebox'></div>
+    )
+}
+
+export default Image;
